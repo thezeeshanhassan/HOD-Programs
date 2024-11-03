8b5e9d1b4f17a38a0d29e5947dcdb856a7c61a84
thezeeshanhassan <thezeeshanhassan@gmail.com>
2024-11-03 19:52:26 +0500

    ADT Like Added

--- DArray/program1.cpp
+++ DArray/program1.cpp
@@ -7,14 +7,35 @@ private:
     int _size;
     int count;
     int *array;
+    void doubleSize();
 
 public:
-    DArray() : _size(1), count(0)
-    {
-        array = new int[_size];
-    }
+    DArray();
+    DArray(const DArray &obj);
+    DArray &operator=(const DArray &obj);
+    int &operator[](int index);
+    void push_back(int val);
+    int size() const;
+};
 
-    DArray(const DArray &obj)
+DArray::DArray() : _size(1), count(0)
+{
+    array = new int[_size];
+}
+DArray::DArray(const DArray &obj)
+{
+    _size = obj._size;
+    count = obj.count;
+    array = new int[obj._size];
+    for (int i = 0; i < obj._size; i++)
+    {
+        array[i] = obj.array[i];
+    }
+}
+
+DArray &DArray::operator=(const DArray &obj)
+{
+    if (this != &obj)
     {
         _size = obj._size;
         count = obj.count;
@@ -24,72 +45,37 @@ public:
             array[i] = obj.array[i];
         }
     }
+    return *this;
+}
 
-    DArray &operator=(const DArray &obj)
+int &DArray::operator[](int index)
+{
+    if (index >= 0 || index < _size)
     {
-        if (this != &obj)
-        {
-            _size = obj._size;
-            count = obj.count;
-            array = new int[obj._size];
-            for (int i = 0; i < obj._size; i++)
-            {
-                array[i] = obj.array[i];
-            }
-        }
-        return *this;
+        return array[index];
+    }
+    cout << "Index Out of Bounds" << endl;
+    static int val{-1};
+    return val;
+}
+
+void DArray::push_back(int val)
+{
+    if (count >= _size)
+    {
+        doubleSize();
     }
 
-     int &operator[](int index)
-    {
-        if (index >= 0 || index < _size)
-        {
-            return array[index];
-        }
-        cout << "Index Out of Bounds" << endl;
-        static int val{-1};
-        return val;
-    }
+    array[count] = val;
+    count += 1;
+}
 
-    void push_back(int val)
-    {
-        if (count >= _size)
-        {
-            doubleSize();
-        }
+int DArray::size() const
+{
+    return count;
+}
 
-        array[count] = val;
-        count += 1;
-    }
-
-    int size() const
-    {
-        return count;
-    }
-
-    void print()
-    {
-        for (int i = 0; i < _size; i++)
-        {
-            cout << array[i] << " ";
-        }
-        cout << endl;
-    }
-
-    void doubleSize()
-    {
-        _size *= 2;
-        int *newArray = new int[_size];
-        for (int i = 0; i < count; i++)
-        {
-            newArray[i] = array[i];
-        }
-        delete[] array;
-        array = newArray;
-    }
-};
-
-ostream &operator<<(ostream &os,  DArray &arr)
+ostream &operator<<(ostream &os, DArray &arr)
 {
     for (int i = 0; i < arr.size(); i++)
     {
@@ -106,6 +92,17 @@ DArray doSomething(DArray arr)
     }
     return arr;
 }
+void DArray::doubleSize()
+{
+    _size *= 2;
+    int *newArray = new int[_size];
+    for (int i = 0; i < count; i++)
+    {
+        newArray[i] = array[i];
+    }
+    delete[] array;
+    array = newArray;
+}
 
 int main()
 {
@@ -117,15 +114,10 @@ int main()
         arr1.push_back(num);
         cin >> num;
     }
-    arr1.print();
-    DArray arr2 = arr1;
-    arr2.print();
-
-    arr2 = doSomething(arr1);
-
-    arr2.print();
-
     cout << arr1 << endl;
-
+    DArray arr2 = arr1;
+    cout << arr2 << endl;
+    arr2 = doSomething(arr1);
+    cout << arr2 << endl;
     return 0;
 }
